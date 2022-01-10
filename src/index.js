4a5f5994d928fe7c6e21a443efe384d6287d2ce2
eu-andreevna <eu.mamarina2014@yandex.ru>
2022-01-10 21:34:17 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+  let numberZeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ];
+  let numberElevenToNinteen = ['eleven','twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let numberDozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let hundred = 'hundred';
+
+  if ( 0 <= number && number <= 9 ) {
+      for (let i = 0; i < numberZeroToNine.length; i += 1) {
+          if (number == i){
+              return numberZeroToNine[i];
+          }
+      }
+  }
 }
